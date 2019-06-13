d9e155474b37e9f5af3480c604d4959cae65cb14
Pavel <pashted9146@gmail.com>
2019-06-13 21:08:14 +0300

    Setup babel configuration

---
+++ babel.config.js
@@ -0,0 +1,25 @@
+const presets = [
+    [
+        "@babel/env",
+        {
+            targets:     {
+                edge:    "17",
+                firefox: "60",
+                chrome:  "67",
+                safari:  "11.1",
+                ie: "10"
+            },
+            useBuiltIns: false,
+        },
+    ],
+    [ "minify" ]
+
+];
+const plugins = [
+    "transform-remove-console"
+];
+
+module.exports = {
+    comments: false,
+    presets, plugins
+};
