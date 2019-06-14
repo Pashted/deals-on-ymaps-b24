8c58789a5752b39066420b92ba6b8b753287c866
Pavel <pashted9146@gmail.com>
2019-06-14 15:36:48 +0300

    Add promise-polyfill for old browsers

--- src/index.js
+++ src/index.js
@@ -16,21 +16,22 @@ require.config({
         '../version':            '../node_modules/jquery-ui/ui/version',
         datepicker_ext:          'jquery.datepicker.extension.range.min',
         bx_api:                  '//api.bitrix24.com/api/v1/?',
-        ymaps_api:               'https://api-maps.yandex.ru/2.1/?lang=ru_RU'
+        ymaps_api:               'https://api-maps.yandex.ru/2.1/?lang=ru_RU',
+        'promise-polyfill':      '../node_modules/promise-polyfill/dist/polyfill.min',
     },
     shim:    {
-        chosen:         { deps: ['jquery'] },
-        datepicker_ext: { deps: ['jquery', '../widgets/datepicker'] }
+        chosen:         { deps: [ 'jquery' ] },
+        datepicker_ext: { deps: [ 'jquery', '../widgets/datepicker' ] }
     }
 });
 
 require(
-    ['uikit', 'uikiticons', 'chosen', 'datepicker_ext', 'bx_api'],
+    [ 'uikit', 'uikiticons', 'chosen', 'datepicker_ext', 'bx_api', 'promise-polyfill' ],
     (UIkit, icons) => {
         icons(UIkit);
 
         BX24.init(() => {
-            require(['settings', 'form', 'ymaps'], (settings, form, map) => {
+            require([ 'settings', 'form', 'ymaps' ], (settings, form, map) => {
                 settings.init()
                     .then(() => {
 
@@ -42,12 +43,12 @@ require(
                                     api = api.replace(/(api-maps)/, 'enterprise.$1');
                             }
                             require(
-                                [api],
+                                [ api ],
                                 () => ymaps.ready(() => form.init()),
                                 err => {
                                     UIkit.modal.alert(err.message);
                                     require(
-                                        ['ymaps_api'],
+                                        [ 'ymaps_api' ],
                                         () => ymaps.ready(() => form.init()),
                                         err => {
                                             UIkit.modal.alert(err.message);
